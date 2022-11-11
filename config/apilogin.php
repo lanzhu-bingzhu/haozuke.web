360d5d53997515b9a5333f17a231f2ee4932c71e
lanzhu-bingzhu <2320697174@qq.com>
2022-11-11 20:53:11 +0800

    隐私性修改

--- config/apilogin.php
+++ config/apilogin.php
@@ -3,7 +3,7 @@ return [
     // 接口使用频率上限
     'click' => 20000,
     // 小程序ID
-    'wx_appid' => 'wxf0a273f21c794a56',
+    'wx_appid' => '',
     // 小程序 appSecret
-    'wx_secret' => '3e24efb10bd0d4182823f6640ceaaa62',
+    'wx_secret' => '',
 ];
